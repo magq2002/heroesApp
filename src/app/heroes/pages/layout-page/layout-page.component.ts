bf238f3954f4190e812690cbfd79ea5aec9ae6b9
AsusMiguel <migalvez2002@gmail.com>
2024-01-07 21:54:15 -0500

    first commit

---
+++ src/app/heroes/pages/layout-page/layout-page.component.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+import { AuthService } from '../../../auth/services/auth.service';
+import { Router } from '@angular/router';
+import { User } from '../../../auth/interfaces/user.interface';
+
+@Component({
+  selector: 'app-layout-page',
+  templateUrl: './layout-page.component.html',
+  styles: ``
+})
+export class LayoutPageComponent {
+  public sidebarItems = [
+    {label: 'Listado', icon: 'label', url: './list'},
+    {label: 'Añadir', icon: 'add', url: './new-hero'},
+    {label: 'Buscar', icon: 'search', url: './search'},
+  ]
+
+  constructor( private authService: AuthService, private router: Router ){}
+
+  get user(): User | undefined {
+    return this.authService.currentUser;
+  }
+  onLogout() {
+    this.authService.logout();
+    this.router.navigate(['/auth/login'])
+  }
+}
